953fd53c698175450394f2fbb02086e4ac720ca9
Agustín <agustin.guzman.v@proton.me>
2025-04-09 17:18:22 -0400

    cambios menores

--- Modulo2_LinkedList/Ejercicios/Medievo/medievo.c
+++ Modulo2_LinkedList/Ejercicios/Medievo/medievo.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 struct Lord {
 	char *nombre;
 	char *apellido;
@@ -28,10 +30,91 @@ struct TronoDeHierro {
 	struct NodoCasa *casas;
 };
 
+float calcularIndicadorLord(struct Lord *lord, int cantidadMilitares) {
+	if (lord == NULL) return 0.0f;
+
+	return lord->batallasGanadas * .2f + lord->oroInvertido * .5f + cantidadMilitares * .3;
+}
+
+struct Lord *getMayorLordEnCasa(struct NodoLord *head, int cantidadMilitares) {
+	if (head == NULL) return NULL;
+
+	struct Lord *lordMayor;
+	float lordMayorIndicador;
+
+	lordMayor = NULL;
+	lordMayorIndicador = 0;
+
+	while (head != NULL) {
+		if (lordMayor == NULL || calcularIndicadorLord(head->datosLord, cantidadMilitares) > lordMayorIndicador) {
+			lordMayor = head->datosLord;
+			lordMayorIndicador = calcularIndicadorLord(head->datosLord, cantidadMilitares);
+		}
+
+		head = head->sig;
+	}
+
+	return lordMayor;
+}
+
 void EliminarReyActual(struct TronoDeHierro *corona) {
 
 }
 
 struct Lord *elegirNuevoRey(struct TronoDeHierro *corona) {
+	if (corona == NULL) return NULL;
 
+	struct Lord *nuevoRey, *candidato;
+	struct Casa *casaGanadora;
+	struct NodoCasa *recCasas;
+	float indicadorNuevoRey, indicadorCandidato;
+
+	nuevoRey = NULL;
+	casaGanadora = NULL;
+	recCasas = corona->casas;
+
+	while (recCasas != NULL) {
+		candidato = getMayorLordEnCasa(recCasas->datosCasa->headLords, recCasas->datosCasa->cantidadMilitares);
+
+		if (candidato == NULL) {
+			recCasas = recCasas->sig;
+			continue;
+		}
+
+		indicadorCandidato = calcularIndicadorLord(candidato, recCasas->datosCasa->cantidadMilitares);
+
+		if (nuevoRey  == NULL ||                        // caso 1: Aún no hay rey asignado
+			indicadorCandidato > indicadorNuevoRey ||   // caso 2: La casa que recorremos supera al actual nuevoRey
+			(indicadorCandidato == indicadorNuevoRey &&	// caso 3: la casa que recorremos empata con el actual nuevoRey
+			recCasas->datosCasa->oroAlmacenado > casaGanadora->oroAlmacenado)) {
+				nuevoRey = candidato;
+				casaGanadora = recCasas->datosCasa;
+				indicadorNuevoRey = indicadorCandidato;
+		}
+
+		/* Versión no compacta
+		// caso 1: Aún no hay rey asignado
+		if (nuevoRey == NULL) {
+			nuevoRey = candidato;
+			casaGanadora = recCasas->datosCasa;
+			indicadorNuevoRey = indicadorCandidato;
+		}
+		// caso 2: La casa que recorremos supera al actual nuevoRey
+		else if (indicadorCandidato > indicadorNuevoRey) {
+			nuevoRey = candidato;
+			casaGanadora = recCasas->datosCasa;
+			indicadorNuevoRey = indicadorCandidato;
+		}
+		// caso 3: la casa que recorremos empata con el actual nuevoRey
+		else if (indicadorCandidato == indicadorNuevoRey && recCasas->datosCasa->oroAlmacenado > casaGanadora->oroAlmacenado)) {
+			nuevoRey = candidato;
+			casaGanadora = recCasas->datosCasa;
+			indicadorNuevoRey = indicadorCandidato;
+		}
+		*/
+
+		recCasas = recCasas->sig;
+	}
+
+	return nuevoRey;
 }
