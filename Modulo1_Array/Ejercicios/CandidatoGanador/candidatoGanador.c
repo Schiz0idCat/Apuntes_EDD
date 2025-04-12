caffbbf9b7707ce4a0cc2a2dfbf9007f871be9a4
Agustín <agustin.guzman.v@proton.me>
2025-04-12 18:21:28 -0400

    no validar parametros, no continue

--- Modulo1_Array/Ejercicios/CandidatoGanador/candidatoGanador.c
+++ Modulo1_Array/Ejercicios/CandidatoGanador/candidatoGanador.c
@@ -15,36 +15,26 @@ struct EleccionesMunicipales {
 };
 
 int contarVotos(struct Candidato **candidatos, int max, char *rut) {
-	if (candidatos == NULL || rut == NULL) return 0;
-
-	int i, totalVotos;
-
-	totalVotos = 0;
+	int i, totalVotos = 0;
 
 	for (i = 0; i < max; i++) {
-		if (candidatos[i] == NULL || candidatos[i]->rut == NULL) continue;
-
-		if (strcmp(candidatos[i]->rut, rut) == 0) totalVotos++;
+		if (candidatos[i] != NULL && strcmp(candidatos[i]->rut, rut) == 0)
+			totalVotos++;
 	}
 
 	return totalVotos;
 }
 
 struct Candidato *determinarAlcaldeGanador(struct Candidato **alcaldes, int pLibreAlcaldes, char *comuna) {
-	if (alcaldes == NULL || comuna == NULL) return NULL;
-
-	int i, votosGanador;
-	struct Candidato *ganador;
-
-	votosGanador = 0;
-	ganador = NULL;
+	int i, votosGanador = 0;
+	struct Candidato *ganador = NULL;
 
 	for (i = 0; i < pLibreAlcaldes; i++) {
-		if (alcaldes[i] == NULL || alcaldes[i]->comunaPostulante == NULL) continue;
-
 		if (strcmp(alcaldes[i]->comunaPostulante, comuna) == 0) {
-			if (ganador == NULL || contarVotos(alcaldes, pLibreAlcaldes, alcaldes[i]->rut) > votosGanador) {
-				votosGanador = contarVotos(alcaldes, pLibreAlcaldes, alcaldes[i]->rut);
+			int votosAlcalde = contarVotos(alcaldes, pLibreAlcaldes, alcaldes[i]->rut);
+
+			if (ganador == NULL || votosAlcalde > votosGanador) {
+				votosGanador = votosAlcalde;
 				ganador = alcaldes[i];
 			}
 		}
