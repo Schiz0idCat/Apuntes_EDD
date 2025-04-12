3d508ef19e6b7225f8d5e27ac7194ff93a79ca82
Agustín <agustin.guzman.v@proton.me>
2025-04-12 18:02:14 -0400

    no más validar los parámetros

--- Modulo1_Array/Ejercicios/Alumnos/alumnos.c
+++ Modulo1_Array/Ejercicios/Alumnos/alumnos.c
@@ -10,28 +10,21 @@ struct Alumno {
 };
 
 int buscarAlumno(struct Alumno **alumnos, int pLibre, char *rut) {
-	if (alumnos == NULL) return 0;
-
 	int i;
 
 	for (i = 0; i < pLibre; i ++) {
-		if (alumnos[i] == NULL || alumnos[i]->rut == NULL) continue;
-
-		if (strcmp(alumnos[i]->rut, rut) == 0) return 0;
+		if (alumnos[i] != NULL || strcmp(alumnos[i]->rut, rut) == 0)
+			return 0;
 	}
 
 	return 1;
 }
 
 int agregarAlumnos(struct Alumno **alumnos, int *pLibre, char *rut, char *nombre, float promedio) {
-	if (alumnos == NULL || rut == NULL || nombre == NULL) return 1;
-
 	// si el array se encuentra lleno o el estudiante ya está en el array, retorna 1
 	if (*pLibre >= ARRAYMAX || buscarAlumno(alumnos, *pLibre, rut) == 0) return 1;
 
-	struct Alumno *nuevoAlumno;
-
-	nuevoAlumno = (struct Alumno *)malloc(sizeof(struct Alumno));
+	struct Alumno *nuevoAlumno = (struct Alumno *)malloc(sizeof(struct Alumno));
 
 	if (nuevoAlumno == NULL) return 1;
 
@@ -39,23 +32,16 @@ int agregarAlumnos(struct Alumno **alumnos, int *pLibre, char *rut, char *nombre
 	nuevoAlumno->nombre = nombre;
 	nuevoAlumno->promedio = promedio;
 
-	alumnos[*pLibre] = nuevoAlumno;
-	(*pLibre)++;
+	alumnos[(*pLibre)++] = nuevoAlumno;
 
 	return 0;
 }
 
 char *getAlumnoMejorPromedio(struct Alumno **alumnos, int pLibre) {
-	if (alumnos == NULL) return NULL;
-
 	int i;
-	struct Alumno *mejorPromedio;
-
-	mejorPromedio = NULL;
+	struct Alumno *mejorPromedio = NULL;
 
 	for (i = 0; i < pLibre; i++) {
-		if (alumnos[i] == NULL || alumnos[i]->rut == NULL) continue;
-
 		if (mejorPromedio == NULL || alumnos[i]->promedio > mejorPromedio->promedio)
 			mejorPromedio = alumnos[i];
 	}
